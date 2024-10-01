21523534e447001f93e38ea00421cde873a5580d
YoungerMax <60242131+YoungerMax@users.noreply.github.com>
2024-10-01 19:34:35 -0400

    update all documentation with note format and add AI summarizer and summary for September

--- README.md
+++ README.md
@@ -2,8 +2,35 @@
 
 This repository contains daily documentation for the 2024-2025 FTC season for T-10.
 
+## Automatic monthly summaries
+
+**You need an API key from Groq. It's completely FREE! [Click here.](https://console.groq.com)**
+
+Run `GROQ_API_KEY=gsk_xxx python3 summarize.py [file pattern] > output-file.txt`
+* `GROQ_API_KEY` is an environment variable that is set to your Groq API key.
+
+Example: `GROQ_API_KEY=gsk_xxx python3 summarize.py 09* > 09-summary.txt`
+- Summarizes all files starting with `09` (September documentation)
+- Writes the summary to the file `09-summary.txt`
+
 ## Conventions
+
+### Format
+
+```
+[A high level summary of the entire meeting, one sentence.]
+
+A. First thing that we did
+B. Second thing that we did
+    1. A sub thing that was happening
+    2. Another sub thing
+        a. A specific detail to that sub thing
+        b. Another specific detail
+    3. Yet another sub thing
+C. Third thing that we did
+```
 
 ### File naming
 
-Daily documentation format: `MMDDYY-comment-goes-here.txt`
+- Daily documentation: `MMDDYY-comment-goes-here-day-#.txt`
+- Monthly documentation summaries: `MM-summary.txt`
---
+++ summarize.py
@@ -0,0 +1,61 @@
+from groq import Groq
+from groq.types.chat import ChatCompletionUserMessageParam, ChatCompletionSystemMessageParam
+import sys
+import os
+from datetime import date
+
+g = Groq()
+
+
+def main():
+    files_to_summarize = sys.argv[1:]
+
+    if not files_to_summarize:
+        print('expected files to summarize in every command line argument, got none')
+        sys.exit(-1)
+    
+    text_file_contents = ''
+    
+    for file in files_to_summarize:
+        if not os.path.exists(file):
+            print('file does not exist:', file)
+            sys.exit(-2)
+        
+        d = date(
+            2000 + int(file[4:6]),
+            int(file[0:2]),
+            int(file[2:4])
+        )
+        
+        text_file_contents += f"{d.strftime('%B %d, %Y')}:\n```\n"
+
+        with open(file, 'rt') as fp:
+            text_file_contents += fp.read()
+        
+        text_file_contents += "```\n\n"
+    
+    completion = g.chat.completions.create(
+        model="llama-3.1-70b-versatile",
+        stream=True,
+        messages=[
+            ChatCompletionSystemMessageParam(
+                role="system",
+                content="""Respond ONLY with a summary the following text files into one, cohensive summary of the entire period (most likely only a month). Write as if you are a member of the team writing this--say "we" did something instead of saying "the team" did something."""
+            ),
+            ChatCompletionUserMessageParam(
+                role="user",
+                content=text_file_contents.strip()
+            ),
+            ChatCompletionSystemMessageParam(
+                role="assistant",
+                content="Here is a cohesive and detailed summary of the entire period:\n\n"
+            )
+        ],
+    )
+
+    for chunk in completion:
+        if chunk.choices[0].delta.content:
+            print(chunk.choices[0].delta.content, end='', flush=True)
+
+if __name__ == "__main__":
+    main()
